7180480b18d679e9c8a9f930e2fb2c0f477b312f
Dinesh <email2dineshpawar@gmail.com>
2019-11-03 13:42:32 +0530

    Initial Selection Setup

---
+++ MyShop/MyShop.webUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.webUI.Startup))]
+namespace MyShop.webUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
