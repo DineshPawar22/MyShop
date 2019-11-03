01edc0051f7f62cd7adf45f754b6eb3b2cd87486
admin <admin@ADMIN-PC>
2019-11-03 20:36:47 +0530

    ProductManager

---
+++ MyShop/MyShop.Core/ProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+using System.Runtime.Caching;
+
+namespace MyShop.Core
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+                products = new List<Product>();
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product ProductToUpdate = products.Find(p => p.Id == product.Id);
+            if (ProductToUpdate != null)
+            {
+                ProductToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+
+        }
+
+        public Product find(string Id)
+        {
+            Product ProductToFind = products.Find(p => p.Id == Id);
+            if (ProductToFind != null)
+            {
+                return ProductToFind;
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+
+        }
+
+
+        public void DeleteProduct(Product product)
+        {
+            Product ProductToDelete = products.Find(p => p.Id == product.Id);
+            if (ProductToDelete != null)
+            {
+                products.Remove(ProductToDelete);
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+    }
+}
---
+++ MyShop/MyShop.UI/Controllers/ProductManagerController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.Core;
+
+namespace MyShop.UI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+            return View();
+        }
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.find(Id);
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                productToEdit.Catageory = product.Catageory;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product product = context.find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult conformDelete(string Id)
+        {
+            Product product = context.find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.DeleteProduct(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
